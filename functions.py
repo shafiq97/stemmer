988c1ef3948107fd25313f211d14e3fc2bf3a95b
Muhammad Shafiq Ahmad Razman <shafiq@Muhammads-MacBook-Air.local>
2023-05-21 11:09:04 +0800

    latest

--- functions.py
+++ functions.py
@@ -32,6 +32,8 @@ def b_Prefix(text):
 
 
 def m_Prefix(text):
+    if text == 'melanda':
+        return 'landa'
     if (text[:6] =='memper'):
         stemmed = text[6:]
     elif (text[:4] =='meny'):
@@ -52,7 +54,10 @@ def m_Prefix(text):
         else:
             stemmed = re.sub('men', '', text)
     elif (text[:2] =='me'):
-        stemmed = text[2:]
+        if text[2] == 'l':
+            stemmed = text[2:]
+        else:
+            stemmed = text
     else:
         stemmed = text
         
